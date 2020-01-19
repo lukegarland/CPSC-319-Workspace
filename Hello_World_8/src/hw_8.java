57075ab8c68e51dff0191796d8669c571313b636
lukegarland <luke.garland01@gmail.com>
2020-01-19 14:04:42 -0700

    Initial commit

---
+++ Hello_World_8/src/hw_8.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;
+
+
+public class hw_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		out.println("Hello World");
+		
+	}
+
+}
