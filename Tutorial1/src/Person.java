83e2cf5cbcd55b972cb7a333386d067f1b4cd070
Luke Garland <luke.garland1@cs.ucalgary.ca>
2020-01-21 13:45:53 -0700

    Tutorial1

---
+++ Tutorial1/src/Person.java
@@ -0,0 +1,27 @@
+
+public class Person {
+
+	
+	private String name;
+	
+	public Person(String name)
+	{
+		this.name = name;
+	}
+	
+	public Person() {
+		name = "No name";
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+		
+	}
+	
+}
