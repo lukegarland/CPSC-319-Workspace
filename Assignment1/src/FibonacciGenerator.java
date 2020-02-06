abdb9ed7b0ecd7ca2c1e1c0d9f20611fb733a8b9
lukegarland <luke.garland01@gmail.com>
2020-02-06 13:26:08 -0700

    Added cost analysis

--- Assignment1/src/FibonacciGenerator.java
+++ Assignment1/src/FibonacciGenerator.java
@@ -10,6 +10,14 @@ import java.util.Arrays;
 public class FibonacciGenerator {
 
 	
+	public static void main(String[] args) {
+		
+		runExperimentalStudyListFormat(60_000_000);
+		//testAlgorithms();
+		//runExperimentalStudyTableFormat(200_000_000, 100);
+	}
+	
+	
 	/**
 	 * Fibonacci number generator that uses the recursive method.
 	 * 
@@ -34,12 +42,12 @@ public class FibonacciGenerator {
 	 */
 	public static long fibMem(int n)
 	{
-		ArrayList<Long>memoCache = new ArrayList<Long>(n);
-		memoCache.add((long) 0);
+		ArrayList<Long>memoCache = new ArrayList<Long>(n); 
+		memoCache.add((long) 0); 
 		memoCache.add((long) 1);
 		
 		if(n <= 1)
-			return memoCache.get(n);
+			return memoCache.get(n); 
 		else 
 		{
 			long value = fibMem(n - 1,memoCache) + fibMem(n - 2, memoCache);
@@ -78,24 +86,29 @@ public class FibonacciGenerator {
 	 */
 	public static long fibDyn(int n)
 	{
-		long[] dynamicArray = new long[n+2];
-		dynamicArray[0] = 0L;
-		dynamicArray[1] = 1L;
+		long[] dynamicArray = new long[n+2];							// cost = 1
+		dynamicArray[0] = 0L;											// cost = 1
+		dynamicArray[1] = 1L;											// cost = 1
 		
-		assert n < dynamicArray.length;
+		assert n < dynamicArray.length;									// cost = 2
 		
-		long value = 0;
-		if (n < 2)
-			value = dynamicArray[n];
+		long value = 0;													// cost = 1
+		
+		if (n < 2)														// cost = 1
+			value = dynamicArray[n]; 									// cost = 2
 		else
-		{	
-			for(int i = 2; i <= n; i++)
+		{																// i = 2 -> cost  = 1
+			for(int i = 2; i <= n; i++)									// cost = 1, n times 
 			{
-				value = dynamicArray[i - 1] + dynamicArray[i - 2];
-				dynamicArray[i] = value;
-			}
+				value = dynamicArray[i - 1] + dynamicArray[i - 2];		// cost = 4, n-1 times
+				dynamicArray[i] = value;								// cost = 4, n-1 times
+			}															// i++ -> cost = 2, n-1 times
 		}
-		return value;
+		
+		return value;													//cost = 1
+		
+																		// TOTAL worst case f(n) = 1+1+1+2+1+1+1+1*(n)+4*(n-1)+4*(n-1)+2*(n-1)+1
+																		// f(n) = 11*n-2
 	}
 
 	/**
@@ -106,24 +119,29 @@ public class FibonacciGenerator {
 	 */
 	public static long fibIter(int n)
 	{
-		long n_1 = 1;
-		long n_2 = 0;
-		long value = 0;
-		
-		if (n == 0)
+		long n_1 = 1; 						// cost = 1
+		long n_2 = 0;						// cost = 1
+		long value = 0;						// cost = 1
+				
+		if (n == 0)							// cost = 1
 			return 0;
-		else if (n == 1)
+		else if (n == 1)					// cost = 1
 			return 1;
-		else
-		{
-			for(int i = 2; i <= n; i++)
+		else								
+		{									// i = 2 -> cost  = 1
+			for(int i = 2; i <= n; i++)		// cost = 1, n times 
 			{
-				value = n_1 + n_2;
-				n_2 = n_1;
-				n_1 = value;
-			}
-			return value;
+				value = n_1 + n_2;			// cost = 2, n-1 times
+				n_2 = n_1;					// cost = 1, n-1 times
+				n_1 = value;				// cost = 1, n-1 times
+			}								// i++ -> cost = 2, n-1 times
+			return value;					// cost = 1
 		}
+		
+		
+											// TOTAL worst case f(n) = 1+1+1+1+1+1+1*n+2*(n-1)+1*(n-1)+1*(n-1)+2*(n-1)+1
+											// f(n) = 7n + 1
+		
 	}
 	
 	/**
@@ -134,19 +152,26 @@ public class FibonacciGenerator {
 	 */
 	public static long fibMat(int n)
 	{
-		if(n == 0)
-			return 0;
-		long [][] FM = new long[2][2];
+		if(n == 0)							// cost = 1
+			return 0;						
+		long [][] FM = new long[2][2];		// cost = 1
 
 		// Initialize FM
-		FM[0][0] = 1;
-		FM[0][1] = 1;
-		FM[1][0] = 1;
-		FM[1][1] = 0;
+		FM[0][0] = 1;						// cost = 1				
+		FM[0][1] = 1;						// cost = 1
+		FM[1][0] = 1;						// cost = 1
+		FM[1][1] = 0;						// cost = 1
+											
 		
-		matrixPower(n-1, FM);
+											// Let running time of matrixPower(n) = g(n)
+		matrixPower(n-1, FM);				// cost = 2 + g(n-1)
+											// 		= 2 + 67*log(n-1) + 1
 		
-		return FM[0][0];		
+		return FM[0][0];					// cost = 1
+		
+											
+											// TOTAL worst case f(n) = 6 + 2 + 67log(n-1) + 1
+											// f(n) = 67log(n-1) + 9
 	}
 	
 	/**
@@ -158,42 +183,50 @@ public class FibonacciGenerator {
 	 */
 	private static void matrixPower(int n,long[][] FM )
 	{
-		if(n > 1)
+		if(n > 1) //	cost = 1 
 		{
-			matrixPower(n/2, FM);
+			matrixPower(n/2, FM); //	cost = g(n/2)
 			
 			//FM = FM * FM 
-			long a = FM[0][0] * FM[0][0]  + FM[0][1] * FM[1][0];
-			long b = FM[0][0] * FM[0][1]  + FM[0][1] * FM[1][1];
-			long c = FM[1][0] * FM[0][0]  + FM[1][1] * FM[1][0];
-			long d = FM[1][0] * FM[0][1]  + FM[1][1] * FM[1][1];
-			FM[0][0] = a;
-			FM[0][1] = b;
-			FM[1][0] = c;
-			FM[1][1] = d;
+			long a = FM[0][0] * FM[0][0]  + FM[0][1] * FM[1][0]; 	//cost = 8
+			long b = FM[0][0] * FM[0][1]  + FM[0][1] * FM[1][1]; 	//cost = 8
+			long c = FM[1][0] * FM[0][0]  + FM[1][1] * FM[1][0]; 	//cost = 8
+			long d = FM[1][0] * FM[0][1]  + FM[1][1] * FM[1][1]; 	//cost = 8
+			FM[0][0] = a; 											//cost = 1
+			FM[0][1] = b; 											//cost = 1
+			FM[1][0] = c; 											//cost = 1
+			FM[1][1] = d; 											//cost = 1
 
-			if(n % 2 != 0)
+			if(n % 2 != 0)											//cost = 2
 			{	//If n is odd -> FM * [1,1][1,0]
-				long a1 = FM[0][0] * 1  + FM[0][1] * 1;
-				long b1 = FM[0][0] * 1  + FM[0][1] * 0;
-				long c1 = FM[1][0] * 1  + FM[1][1] * 1;
-				long d1 = FM[1][0] * 1  + FM[1][1] * 0;
-				FM[0][0] = a1;
-				FM[0][1] = b1;
-				FM[1][0] = c1;
-				FM[1][1] = d1;
+				long a1 = FM[0][0] * 1  + FM[0][1] * 1; 			// cost = 6
+				long b1 = FM[0][0] * 1  + FM[0][1] * 0;				// cost = 6
+				long c1 = FM[1][0] * 1  + FM[1][1] * 1;				// cost = 6
+				long d1 = FM[1][0] * 1  + FM[1][1] * 0;				// cost = 6
+				FM[0][0] = a1;										// cost = 1
+				FM[0][1] = b1;										// cost = 1
+				FM[1][0] = c1;										// cost = 1
+				FM[1][1] = d1;										// cost = 1
 			}
 		}
-	}
+		
+																	// g(n) = 1 			if n = 1
+																	// 		= g(n/2)+67 	otherwise
+																	
+																	// by using substitution ... g(n) = g(n/2^k) + 67k, and log(n) = k
+																	// g(n) = 67*log(n) + 1
+	}	
 	
 	/**
 	 * Runs an experimental study of the runtimes for fibRec(n), fibMem(n), fibDyn(n), fibIter(n), and fibMat(n).
 	 * Will print out the average execution time in ns for some values of n up to maxValue.
+	 * n will step up by 1 until n == 40, then n = 100, then n doubles each time thereafter.
+	 * For FibRec(n), n will never exceed 40, and for FibMem(n) n will never exceed 6400. All other functions are guaranteed to print up to (but not including maxValue) 
 	 * 
 	 * @param maxValue maximum fibonacci value to calculate
 	 * @param numberOfCalls number of times each algorithm will be called before n is incremented
 	 */
-	private static void runExperimentalStudy(int maxValue, int numberOfCalls)
+	private static void runExperimentalStudyTableFormat(int maxValue, int numberOfCalls)
 	{
 		long [] results = new long[5];
 
@@ -260,6 +293,112 @@ public class FibonacciGenerator {
 	
 	
 	/**
+	 * Prints the running time for fibRec(n), fibMem(n), fibDyn(n), fibIter(n), and fibMat(n) in a list for certain values of n.
+	 * n will step up by 1 until n == 40, then n = 100, then n doubles each time thereafter.
+	 * For FibRec(n), n will never exceed 40, and for FibMem(n) n will never exceed 6400. All other functions are guaranteed to print up to (but not including maxValue) 
+	 * 
+	 * 
+	 * @param maxValue maximum input value
+	 */
+	private static void runExperimentalStudyListFormat(int maxValue)
+	{
+		int n;
+		long timeStart;
+		
+		
+		System.out.println("n       time");
+		
+		//Recursive
+		n = 0;
+		while(n < maxValue && n <= 40) //Over n = 40 fibRec takes too long
+		{
+			
+		
+			timeStart = System.nanoTime();
+			fibRec(n);
+			System.out.printf("%-8d %d\n", n, System.nanoTime() - timeStart);
+
+			n++;
+			
+		}
+		
+		//Memoization
+		n = 0;
+		System.out.println();
+		while(n < maxValue &&  n < 6400) //Do not run memoization after 6400 as StackOverFlowError exception will be thrown for n = 6400
+		{
+			
+
+			timeStart = System.nanoTime();
+			fibMem(n);
+			System.out.printf("%-8d %d\n", n, System.nanoTime() - timeStart);
+			
+			if(n < 40)
+				n++;
+			else if (n < 100)
+				n = 100;
+			else 
+				n *= 2;
+		}
+		
+		//Dynamic
+		n = 0;
+		System.out.println();
+		while(n < maxValue)
+		{
+
+			timeStart = System.nanoTime();
+			fibDyn(n);
+			System.out.printf("%-8d %d\n", n, System.nanoTime() - timeStart);
+			
+			if(n < 40)
+				n++;
+			else if (n < 100)
+				n = 100;
+			else 
+				n *= 2;
+		}
+		
+		//Iteration
+		n = 0;
+		System.out.println();
+		while(n < maxValue)
+		{
+
+			timeStart = System.nanoTime();
+			fibIter(n);
+			System.out.printf("%-8d %d\n", n, System.nanoTime() - timeStart);
+			
+			if(n < 40)
+				n++;
+			else if (n < 100)
+				n = 100;
+			else 
+				n *= 2;
+		}
+		
+		//Matrix
+		n = 0;
+		System.out.println();
+		while(n < maxValue)
+		{
+
+			timeStart = System.nanoTime();
+			fibMat(n);
+			System.out.printf("%-8d %d\n", n, System.nanoTime() - timeStart);
+			
+			if(n < 40)
+				n++;
+			else if (n < 100)
+				n = 100;
+			else 
+				n *= 2;
+		}
+		System.out.println();
+
+	}
+	
+	/**
 	 * Tests various fibonacci algorithms.
 	 */
 	private static void testAlgorithms() 
@@ -269,11 +408,6 @@ public class FibonacciGenerator {
 		testfibDyn();
 		testFibIter();	
 		testFibMat();
-	}
-	
-	public static void main(String[] args) {
-		testAlgorithms();
-		runExperimentalStudy(300_000_000, 100);
 	}
 	
 	
---
+++ Assignment1/src/README.md
@@ -0,0 +1,24 @@
+# Assignment 1
+
+## Build instructions
+
+```bash
+cd src
+make
+```
+
+It should compile all the java source files that need to be re-built. The build has been tested with GNU compiler Linux (Fedora 26).
+
+## Usage
+
+From the Command Prompt, type "java" with a class name of the program.
+
+```bash
+java FibonacciGenerator
+```
+
+If you want to test the output of each algorithm, in the ```main``` method, run the static method ```testAlgorithms()```.
+
+To print a table of the average running time of each algorithm after a number of function calls up to a certain value of n, run the method ```runExperimentalStudyTableFormat(int maxValue, int numberOfCalls)```.
+
+For more documentation, view the javadocs page at ```doc/FibonacciGenerator.html```
